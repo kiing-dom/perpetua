0876693dc8215773719a42a8bd2cd7ff7968502e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-16 01:56:40 +0000

    separated client and server components in root layout

---
+++ src/app/RootLayout.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { useEffect, useState } from "react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setDarkMode(true);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    const html = document.documentElement;
+    if (html.classList.contains("dark")) {
+      localStorage.setItem("theme", "light");
+      setDarkMode(false);
+    } else {
+      html.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setDarkMode(true);
+    }
+  };
+
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-custom-grid`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
