d72dda8edc430c9f1747c464f86fe597a8f02792
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-17 07:58:12 +0000

    dark mode toggle util

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+      <h1 className="dark:text-white text-black">Welcome to Perpetua</h1>
+      <p className="text-black">This is where the development begins</p>
     </div>
   );
 }
