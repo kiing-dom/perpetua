6f3612c13ba90f9acd8c3ef043d6d41b00b65ac0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-17 09:39:57 +0000

    created footer component

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import { FaGithub, FaYoutube } from "react-icons/fa";
+
+const socials = [
+    {
+        link: "https://www.youtube.com/@1KIINGDOM?sub_confirmation=1",
+        label: "YouTube",
+        Icon: FaYoutube
+    },
+    {
+        link: "https://www.github.com/kiing-dom/perpetua",
+        label: "GitHub",
+        Icon: FaGithub
+    }
+]
+
+export default function Footer() {
+    return (
+        <footer className="bg-[d8d8ff] bg-opacity-50 rounded-lg text-white py-6 mt-8">
+            <div className="container mx-auto flex flex-col items-center">
+                <div className="flex items-center gap-4 mb-4">
+                    {socials.map((social, i) => {
+                        const Icon = social.Icon;
+
+                        return (
+                            <Link href={social.link} key={i} aria-label={social.label} legacyBehavior>
+                                <a target="_blank" rel="noopener noreferrer">
+                                    <Icon className="w-8 h-8 hover:scale-110 transition-all"/>
+                                </a>
+                            </Link>
+                        )
+                    })}
+                </div>
+                <p className="text-sm">&copy; Dominion Gbadamosi {new Date().getFullYear()}</p>
+            </div>
+        </footer>
+    )
+}
