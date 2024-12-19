45599d8ab89024b7cdb9b19fc3464552d1f5dd04
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-19 01:21:53 +0000

    notes adding to dashboard complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,39 @@
+"use client"
+
+import { title } from "process";
+import useNotesStore from "../../store/useNotesStore";
 
 export default function Dashboard() {
+    const {notes, addNote } = useNotesStore();
+
+    const handleAddNote = () => {
+        const newNote = {
+            id: `${Date.now()}`,
+            title: `Note ${notes.length + 1}`,
+            content: `This is a new note!`,
+        };
+        addNote(newNote);
+    }
+
     return (
         <div className="min-h-screen p-6 my-6">
             <h1 className="text-2xl font-bold mb-4 dark:text-white text-neutral-600">My Notes</h1>
-            {/* Placeholder for notes here */}
-
+            <button
+                onClick={handleAddNote}
+                className=""
+            >
+                Add Note
+            </button>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                <div className="p-4 bg-white rounded shadow">Placeholder 1</div>
-                <div className="p-4 bg-white rounded shadow">Placeholder 2</div>
+                {notes.map((note) => (
+                    <div
+                        key={note.id}
+                        className=""
+                    >
+                        <h2 className="text-lg font-bold">{note.title}</h2>
+                        <p className="text-md">{note.content}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
