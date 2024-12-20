47368253d5a6532b7e917697a979c09ed32e7c2f
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-20 22:04:02 +0000

    added register to the auth helper

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const auth = getAuth();
+
+export const register = async (email: string, password: string) => {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+};
 
 export const login = async (email: string, password: string) => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
