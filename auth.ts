41d453b91bc25bc2f7b7fdba5fc746ed0a8b6757
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-25 22:44:59 +0000

    exported firebase auth from firebase auth helper

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-const auth = getAuth();
+export const auth = getAuth();
 
 export const register = async (email: string, password: string) => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
